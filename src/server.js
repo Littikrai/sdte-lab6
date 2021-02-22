ad37afa4dfef08423f868bb095b54258373fff61
oil <oilthepop@gmail.com>
2021-02-22 14:34:43 +0700

    updates debug server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hi", (req, res) => {
-  res.send("Hello This This");
+  res.send("Hello World!");
 });
 
 module.exports = app;
